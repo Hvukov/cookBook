1c6e9916fbacf0a0b356cb1dda39637df0493469
veronika-simic <veronikasimic21@gmail.com>
2023-02-23 11:10:46 +0100

    create interface for Recipe and Ingridient type

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,37 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Recipe } from '../Recipe';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
-export interface Config {
-  recepiesUrl: string;
-}
-
-@Injectable()
-export class ConfigService {
-  configUrl =
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  private apiURL =
     'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=50b92134&app_key=2fad6852920d5b6c6682d26c8167cf00';
-  constructor(private http: HttpClient) {}
-  getConfig() {
-    return this.http
-      .get<Config>(this.configUrl)
-      .pipe(retry(3), catchError(this.handleError));
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  constructor(private httpClient: HttpClient) {}
+  getRecipes(): Observable<Recipe[]> {
+    return this.httpClient
+      .get<Recipe[]>(this.apiURL)
+      .pipe(catchError(this.errorHandler));
   }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error('An error occurred:', error.error);
+  getRecipesByID(id: number): Observable<Recipe> {
+    return this.httpClient
+      .get<Recipe>(this.apiURL + id)
+      .pipe(catchError(this.errorHandler));
+  }
+  errorHandler(error: {
+    error: {
+      message: string;
+    };
+    status: any;
+    message: any;
+  }) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
     } else {
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-
-    return throwError(
-      () => new Error('Something bad happened; please try again later.')
-    );
+    return throwError(errorMessage);
   }
 }

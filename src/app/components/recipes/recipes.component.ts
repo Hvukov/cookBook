cbd405f008bc0e65ad763fe6ef20ccc4328a9f4e
Hvukov <hrvoje.7979@gmail.com>
2023-02-24 15:41:15 +0100

    reorganize folder structure

---
+++ src/app/components/recipes/recipes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../../services/config.service';
+import { Recipe } from '../../Recipe';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css'],
+})
+export class RecipesComponent implements OnInit {
+  recipes: Array<Recipe> = [];
+  constructor(private recipeService: RecipeService) {}
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe(data => {
+      this.recipes = data;
+      console.log(this.recipes);
+    });
+  }
+}

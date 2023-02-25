45fb8705ad2f4b2118431ceadc096a74f6289ed5
veronika-simic <veronikasimic21@gmail.com>
2023-02-25 10:40:02 +0100

    hamburger menu spans the whole width

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
+import { ViewEncapsulation } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent {}

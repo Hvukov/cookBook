c221ea67ab44a13405b562adfdbdabf687501679
Hvukov <hrvoje.7979@gmail.com>
2023-02-28 12:12:14 +0100

    add routing to home page and set it to default when page loads

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CookComponent } from './components/navbar/cook/cook.component';
 import { TrendingComponent } from './components/navbar/trending/trending.component';
 import { AboutComponent } from './components/navbar/about/about.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
   { path: 'cook', component: CookComponent },
   { path: 'trending', component: TrendingComponent },
   { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({

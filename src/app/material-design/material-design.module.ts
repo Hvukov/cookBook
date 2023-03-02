0678315486c722f601943473aa18944570a44494
Hvukov <hrvoje.7979@gmail.com>
2023-03-02 10:11:26 +0100

    add stepper without logic

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -8,6 +8,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /**
  *@description Module for Material Design. Instead of using App Module we use this one for the Material Design.
@@ -23,6 +27,10 @@ import { MatStepperModule } from '@angular/material/stepper';
     MatMenuModule,
     MatIconModule,
     MatStepperModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    ReactiveFormsModule,
   ],
 })
 export class MaterialDesignModule {}
